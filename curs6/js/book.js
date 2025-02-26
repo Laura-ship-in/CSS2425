109c18324828203c74c46123bdd5fedac74677df
Laura <elaura39@yahoo.com>
2025-02-26 20:44:29 +0200

    curs6

---
+++ curs6/js/book.js
@@ -0,0 +1,14 @@
+function Book(title, author, page) {
+    this.title = title;
+    this.author = author;
+    this.page=page;
+    this.toString = this.toString 
+
+}
+function toString() {
+    return ` ${this.title}  ${this.author}, si are  ${this.page} pagini`;
+}
+function citeste() {
+    return 'Acum citesc o carte';
+}
+export{Book, citeste};
---
+++ curs6/js/main2.js
@@ -0,0 +1,4 @@
+import Carte, {citeste} from './book2.js';
+let carte = new Carte('Poezii', 'Mihai Eminescu', 250);
+console.log(carte.toString());  
+console.log(stau());
